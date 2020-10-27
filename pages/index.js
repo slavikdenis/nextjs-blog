c771b032b8247aa1cc0352aeb104a0be14f9c59d
Denis Slavik <denis.slavik@gmail.com>
2020-10-27 23:32:37 +0100

    Use yarn instead of npm

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <p>Hi! I'm Denis and I'm doing the nextjs 'Get started' tasks {':)'}</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <a href="https://nextjs.org/learn" target="_blank">our Next.js tutorial</a>.)
         </p>
       </section>
     </Layout>
